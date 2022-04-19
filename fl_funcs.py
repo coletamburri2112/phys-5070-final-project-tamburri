4e13f190160a4aba6343c526f68644de617d2270
coletamburri2112 <cole.tamburri@colorado.edu>
2022-04-19 14:32:43 -0600

    Minor fl_funcs documentation changes

--- fl_funcs.py
+++ fl_funcs.py
@@ -7,37 +7,7 @@ Created on Tue Mar 22 07:09:33 2022
 """
 # Most recent version: 21 March 2022 by Cole Tamburri
 # University of Colorado Boulder
-# Pre-existing code for Computational Physics Project, Spring 2022
-
-# Major working functions for analysis of SDO/EVE 304 Angstrom light curves,
-# SDO/AIA 1600 Angstrom images and ribbon masks, and SDO/HMI magnetograms.
-# Includes algorithm for determination of separation and elongation of both
-# ribbons relative to the polarity inversion line. Flares able to beanalyzed
-# are contained in the RibbonDB database (Kazachenko et al. 2017).  Prior to
-# running this script, the user should obtain flare light curves and times
-# corresponding to the modeled flares in this database, for which the
-# impulsiveness index has been determined previously.
-
-# The polarity inversion line is determined by convolving the HMI masks
-# associated for each flare with a Gaussian of predetermined length, then
-# finding the major region of intersection between these and using the
-# resulting heatmap to fit a fourth-order polynomial.  Details of separation
-# and elongation methods relative to the PIL are included below.
-
-# Reconnection rates and ribbon areas for both positive and negative ribbons
-# are determined, and the values corresponding to the rise phase of the flare
-# (with some flare-specific variation) are fit to an exponential model, in
-# order to prepare for modeling efforts of flare heating particularly in the
-# rise phase.
-
-# Separation and elongation values (perpendicular and parallel PIL-relative
-# motion, respectively) are used to find separation and elongation rates,
-# through which significant periods of these two phases of ribbon motion can
-# be identified.
-
-# Plotting and data presentation routines are also below, which includes an
-# animation showing the timing of separation, elongation, and chromospheric
-# line light curves.
+# Includes re-existing code for Computational Physics Project, Spring 2022
 
 # Modification: 19 April 2022 - cleanup of existing code
 # Modification: 19 April 2022 - development of code to determine shear between
